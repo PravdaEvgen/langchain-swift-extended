d91bb9bd65961c9d2b60f25fa3ed40de83a028b9
Yevhen Pravda <pravdaievgena@gmail.com>
2023-11-10 19:41:29 +0200

    add open properties

--- Sources/LangChain/tools/BaseTool.swift
+++ Sources/LangChain/tools/BaseTool.swift
@@ -17,11 +17,11 @@ public protocol Tool {
     func _run(args: String) async throws -> String
 }
 open class BaseTool: NSObject, Tool {
-    static let TOOL_REQ_ID = "tool_req_id"
-    static let TOOL_COST_KEY = "cost"
-    static let TOOL_NAME_KEY = "tool_name"
-    let callbacks: [BaseCallbackHandler]
-    init(callbacks: [BaseCallbackHandler] = []) {
+    open static let TOOL_REQ_ID = "tool_req_id"
+    open static let TOOL_COST_KEY = "cost"
+    open static let TOOL_NAME_KEY = "tool_name"
+    open let callbacks: [BaseCallbackHandler]
+    open init(callbacks: [BaseCallbackHandler] = []) {
         var cbs: [BaseCallbackHandler] = callbacks
         if Env.addTraceCallbak() && !cbs.contains(where: { item in item is TraceCallbackHandler}) {
             cbs.append(TraceCallbackHandler())
@@ -29,7 +29,7 @@ open class BaseTool: NSObject, Tool {
 //        assert(cbs.count == 1)
         self.callbacks = cbs
     }
-    func callStart(tool: BaseTool, input: String, reqId: String) {
+    open func callStart(tool: BaseTool, input: String, reqId: String) {
         do {
             for callback in callbacks {
                 try callback.on_tool_start(tool: tool, input: input, metadata: [BaseTool.TOOL_REQ_ID: reqId, BaseTool.TOOL_NAME_KEY: tool.name()])
@@ -39,7 +39,7 @@ open class BaseTool: NSObject, Tool {
         }
     }
     
-    func callEnd(tool: BaseTool, output: String, reqId: String, cost: Double) {
+    open func callEnd(tool: BaseTool, output: String, reqId: String, cost: Double) {
         do {
             for callback in callbacks {
                 try callback.on_tool_end(tool: tool, output: output, metadata: [BaseTool.TOOL_REQ_ID: reqId, BaseTool.TOOL_COST_KEY: "\(cost)", BaseTool.TOOL_NAME_KEY: tool.name()])
@@ -49,19 +49,19 @@ open class BaseTool: NSObject, Tool {
         }
     }
     
-    public func name() -> String {
+    open func name() -> String {
         ""
     }
     
-    public func description() -> String {
+    open func description() -> String {
         ""
     }
     
-    public func _run(args: String) async throws -> String {
+    open func _run(args: String) async throws -> String {
         ""
     }
     
-    public func run(args: String) async throws -> String {
+    open func run(args: String) async throws -> String {
         let reqId = UUID().uuidString
         var cost = 0.0
         let now = Date.now.timeIntervalSince1970
